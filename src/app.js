a5e107f3f1d52176c2d867b144d8caefc962f2c7
Luis Miguel Del Valle <150975497+lumi-tip@users.noreply.github.com>
2024-01-29 23:45:13 +0000

    🚀Card Generator Finished

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const symbols = ["&hearts;", "&clubs;", "&spades;", "&diamond;"];
+  const values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  const cardVal = document.getElementById("card-value");
+  const topSymEl = document.querySelector(".top-sym");
+  const bottomSymEl = document.querySelector(".bottom-sym");
+
+  let cardSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+
+  cardVal.innerHTML = values[Math.floor(Math.random() * values.length)];
+
+  if (cardSymbol == "&hearts;") {
+    topSymEl.style.color = "red";
+    bottomSymEl.style.color = "red";
+  }
+
+  topSymEl.innerHTML = cardSymbol;
+  bottomSymEl.innerHTML = cardSymbol;
 };
